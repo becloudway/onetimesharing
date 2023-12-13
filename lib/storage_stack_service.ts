c1cbc222c01e561b8656bfaa9e8f8f2548c8e427
Jensen Bollen <bollenjensen@gmail.com>
2023-12-13 14:18:13 +0100

    Added DynamoDB to the storage stack

--- lib/storage_stack_service.ts
+++ lib/storage_stack_service.ts
@@ -1,9 +1,13 @@
 import { Construct } from "constructs";
-import * as apigateway from "aws-cdk-lib/aws-apigateway";
-import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 
 export class StorageStackService extends Construct {
     constructor(scope: Construct, id: string) {
         super(scope, id);
+
+        const table = new dynamodb.TableV2(this, 'Secret DynamoDB', {
+            partitionKey: { name: 'pk', type: dynamodb.AttributeType.STRING },
+            tableName: "bolleje-dev-dynamodb"
+          });
     }
 }
