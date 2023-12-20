55ff84d43c16c743e94089de6698dc78abdfd493
Jensen Bollen <bollenjensen@gmail.com>
2023-12-20 15:36:43 +0100

    Github Actions

--- lib/cicd_stack_service.ts
+++ lib/cicd_stack_service.ts
@@ -1,5 +1,6 @@
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
 import { RemovalPolicy } from "aws-cdk-lib";
 
 export class CiCdStackService extends Construct {
@@ -9,6 +10,12 @@ export class CiCdStackService extends Construct {
 		const bucket = new s3.Bucket(this, "bolleje-dev-s3-codestorage", {
 			bucketName: "bolleje-dev-s3-codestorage",
 			removalPolicy: RemovalPolicy.DESTROY,
+		});
+
+		new s3deploy.BucketDeployment(this, "DeployWebsite", {
+			sources: [s3deploy.Source.asset("resources/dist")],
+			destinationBucket: bucket,
+			destinationKeyPrefix: "web/static", // optional prefix in destination bucket
 		});
 	}
 }
