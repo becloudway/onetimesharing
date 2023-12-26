0c63d47e74d72526e62ff640158495a82054478b
Jensen Bollen <bollenjensen@gmail.com>
2023-12-26 14:05:39 +0100

    Removed unused imports

--- app/src/SecretEncryption.tsx
+++ app/src/SecretEncryption.tsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import { PublicKey } from "openpgp";
+import React, { useState } from "react";
 import styled from "styled-components";
 import CloudWayLogo from "./assets/logo.png";
 
