f879970af9cf96e8806525f84c8dbc53e8ee88c3
Jensen Bollen <bollenjensen@gmail.com>
2024-01-15 09:43:21 +0100

    Added the icons of the navigation page

--- app/src/Navigation.tsx
+++ app/src/Navigation.tsx
@@ -3,6 +3,10 @@ import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import OneTimeSharingLogo from "./assets/logo.png";
 
+import { ReactComponent as OneClickIcon } from "./assets/oneclickicon.svg";
+import { ReactComponent as PKIIcon } from "./assets/pkiicon.svg";
+import { ReactComponent as KeyIcon } from "./assets/keypairicon.svg";
+
 const Navigation = () => {
 	const navigate = useNavigate();
 
@@ -13,28 +17,31 @@ const Navigation = () => {
 				<div className="flex flex-col gap-[20px] max-w-[600px] w-full px-[10px]">
 					<div className="flex flex-wrap gap-[20px]">
 						<button
-							className="w-full mx-auto max-w-[278px] h-[120px] bg-white rounded-md shadow border border-stone-300"
+							className="w-full mx-auto max-w-[278px] h-[120px] bg-white rounded-md shadow border border-stone-300 flex justify-center items-center gap-[12px]"
 							onClick={() => {
 								navigate("/encryptshe");
 							}}
 						>
-							SHE encryption
+							<OneClickIcon />
+							OneClick sharing
 						</button>
 						<button
-							className="w-full mx-auto max-w-[278px] h-[120px] bg-white rounded-md shadow border border-stone-300"
+							className="w-full mx-auto max-w-[278px] h-[120px] bg-white rounded-md shadow border border-stone-300 flex justify-center items-center gap-[12px]"
 							onClick={() => {
 								navigate("/encrypt");
 							}}
 						>
+							<PKIIcon />
 							PKI encryption
 						</button>
 					</div>
 					<button
-						className="w-full h-[120px] bg-white rounded-md shadow border border-stone-300"
+						className="w-full h-[120px] bg-white rounded-md shadow border border-stone-300 flex justify-center items-center gap-[12px]"
 						onClick={() => {
 							navigate("/keygenerator");
 						}}
 					>
+						<KeyIcon />
 						Key Generator
 					</button>
 				</div>
