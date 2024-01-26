4f337e6cfa56b3d547dc7c330266a0348b1e802e
Jensen Bollen <bollenjensen@gmail.com>
2024-01-26 15:07:28 +0100

    Added the environment name in a env variable.

--- infra/bin/onetimesharing-temporarysecret-cdk.ts
+++ infra/bin/onetimesharing-temporarysecret-cdk.ts
@@ -13,22 +13,22 @@ const AWS_ENVIRONMENT = {
 
 const app = new cdk.App();
 
-const ProdStorageStack = new OneTimeSharingStorageStack(app, "OneTimeSharingProdStorageStack", {
-	environmentName: "prod",
+const ProdStorageStack = new OneTimeSharingStorageStack(app, "OneTimeSharingStorageStack", {
+	environmentName: process.env.ENVIRONMENT || "",
 	env: AWS_ENVIRONMENT,
 });
-const ProdCiCdStack = new OneTimeSharingCiCdStack(app, "OneTimeSharingProdCiCdStack", {
-	environmentName: "prod",
+const ProdCiCdStack = new OneTimeSharingCiCdStack(app, "OneTimeSharingCiCdStack", {
+	environmentName: process.env.ENVIRONMENT || "",
 	env: AWS_ENVIRONMENT,
 });
-const ProdApiStack = new OneTimeSharingApiStack(app, "OneTimeSharingProdApiStack", {
+const ProdApiStack = new OneTimeSharingApiStack(app, "OneTimeSharingApiStack", {
+	environmentName: process.env.ENVIRONMENT || "",
 	DynamoDBStorage: ProdStorageStack.DynamoDBStorage,
-	environmentName: "prod",
 	S3Storage: ProdStorageStack.S3Storage,
 	env: AWS_ENVIRONMENT,
 });
-const ProdFrontendStack = new OneTimeSharingFrontendStack(app, "OneTimeSharingProdFrontendStack", {
-	environmentName: "prod",
+const ProdFrontendStack = new OneTimeSharingFrontendStack(app, "OneTimeSharingFrontendStack", {
+	environmentName: process.env.ENVIRONMENT || "",
 	apiGateway: ProdApiStack.ApiGateway,
 	env: AWS_ENVIRONMENT,
 });
