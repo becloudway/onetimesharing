0750e43266fd64ad738a62ed15be04878a66fdbd
Jensen Bollen <bollenjensen@gmail.com>
2024-01-30 14:41:09 +0100

    Added the screens into the SHE-decryption page and E2E-decryption page.

--- app/src/components/VerifyScreen.tsx
+++ app/src/components/VerifyScreen.tsx
@@ -4,8 +4,12 @@ type VerifyScreenProps = {
 	callback: () => void;
 };
 
-const VerifyScreen = () => {
-	return <div className="fixed top-0 right-0 left-0 bottom-0 bg-red-500"></div>;
+const VerifyScreen = ({ callback }: VerifyScreenProps) => {
+	return (
+		<div className="fixed z-50 top-0 right-0 left-0 bottom-0 bg-red-500">
+			<button onClick={callback}>Close</button>
+		</div>
+	);
 };
 
 export default VerifyScreen;
