7ceb802b63d9b1404a03ec1b299e5c1c26e44a63
Jensen Bollen <bollenjensen@gmail.com>
2024-02-20 09:22:51 +0100

    Removed the Cookies file.

--- app/src/Navigation.tsx
+++ app/src/Navigation.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import OneTimeSharingLogo from "./assets/logo.png";
@@ -6,34 +6,23 @@ import OneTimeSharingLogo from "./assets/logo.png";
 import { ReactComponent as OneClickIcon } from "./assets/oneclickicon.svg";
 import { ReactComponent as PKIIcon } from "./assets/pkiicon.svg";
 import { ReactComponent as KeyIcon } from "./assets/keypairicon.svg";
-import { Cookies } from "./classes/Cookies";
 
 const Navigation = () => {
 	const navigate = useNavigate();
-	const [loggedIn, setLoggedIn] = useState<boolean>(false);
-
-	useEffect(() => {
-		const cookie = Cookies.getCookie("id_token");
-		console.log(cookie);
-		setLoggedIn(cookie !== undefined ? true : false);
-	}, []);
 
 	return (
 		<Container>
 			<div className="flex flex-col items-center justify-center gap-[40px] pt-[34px] w-full h-full bg-[rgba(0,123,236,0.1)]">
 				<img className="h-[40px]" src={OneTimeSharingLogo} />
 				<div className="flex flex-col gap-[20px] max-w-[600px] w-full px-[10px]">
-					{!loggedIn && (
-						<button
-							className="w-full h-[60px] bg-white rounded-md shadow border border-stone-300 flex justify-center items-center gap-[12px]"
-							onClick={() => {
-								navigate("/callback");
-							}}
-						>
-							Login
-						</button>
-					)}
-					{loggedIn && <div className="text-center font-bold text-[18px]">You are logged in</div>}
+					<button
+						className="w-full h-[60px] bg-white rounded-md shadow border border-stone-300 flex justify-center items-center gap-[12px]"
+						onClick={() => {
+							navigate("/callback");
+						}}
+					>
+						Login
+					</button>
 					<div className="flex flex-wrap gap-[20px]">
 						<button
 							className="w-full mx-auto max-w-[278px] h-[120px] bg-white rounded-md shadow border border-stone-300 flex justify-center items-center gap-[12px]"
